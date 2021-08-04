9f00f814e9bd9513e24c5408460737973c16732f
Edgar Mendez <edgar.mdz.avz@gmail.com>
2021-08-03 21:36:09 -0500

    adding class BaseModel and renaming GoogleUser class to User

---
+++ Homence Smart Device/Homence Smart Device/Models/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Homence_Smart_Device.Models
+{
+    public class BaseModel: INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
---
+++ Homence Smart Device/Homence Smart Device/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homence_Smart_Device.Models
+{
+    public class User : BaseModel
+    {
+        private string name;
+        private string email;
+        private Uri picture;
+
+        public string Name { get => name; set { name = value; OnPropertyChanged(); } }
+        public string Email { get => email; set { email = value; OnPropertyChanged(); } }
+        public Uri Picture { get => picture; set { picture = value; OnPropertyChanged(); } }
+    }
+    public interface IGoogleManager
+    {
+        void Login(Action<User, string> OnLoginComplete);
+
+        void Logout();
+    }
+}
